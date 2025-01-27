fbb9e85d50135b5a047c7cee861ae1500143ebba
yeshaswiniT <yeshaswini.teella@gmail.com>
2025-01-26 21:17:33 -0500

    commit

--- src/Component/Cal.js
+++ src/Component/Cal.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import buttonData from './Button'; // import the renamed button data
+import './index.css';
 
 const Button = ({ label, onClick, type }) => {
   return (
